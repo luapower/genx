1ceda93457a8b17ea60adadf489ef8bee7e4f87e
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-19 23:30:30 +0200

    osx32 build

---
+++ csrc/genx/build-osx32.sh
@@ -0,0 +1,1 @@
+gcc -arch i386 -O2 genx.c charProps.c -shared -o ../../bin/osx32/libgenx.dylib -Wall -pedantic
--- csrc/genx/build-osx64.sh
+++ csrc/genx/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -O2 genx.c charProps.c -shared -o ../../bin/osx64/libgenx.dylib -Wall -pedantic
+gcc -arch x86_64 -O2 genx.c charProps.c -shared -o ../../bin/osx64/libgenx.dylib -Wall -pedantic
