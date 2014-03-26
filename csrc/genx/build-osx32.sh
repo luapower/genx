24fb64a4eb42ef9286249c0444f31c7513f392b7
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2014-03-26 20:07:05 +0200

    rebuild for osx with @loader_path-based install_name

--- csrc/genx/build-osx32.sh
+++ csrc/genx/build-osx32.sh
@@ -1,1 +1,1 @@
-gcc -arch i386 -O2 genx.c charProps.c -shared -o ../../bin/osx32/libgenx.dylib -Wall -pedantic
+gcc -arch i386 -O2 genx.c charProps.c -shared -install_name @loader_path/libgenx.dylib -o ../../bin/osx32/libgenx.dylib -Wall -pedantic
--- csrc/genx/build-osx64.sh
+++ csrc/genx/build-osx64.sh
@@ -1,1 +1,1 @@
-gcc -arch x86_64 -O2 genx.c charProps.c -shared -o ../../bin/osx64/libgenx.dylib -Wall -pedantic
+gcc -arch x86_64 -O2 genx.c charProps.c -shared -install_name @loader_path/libgenx.dylib -o ../../bin/osx64/libgenx.dylib -Wall -pedantic
